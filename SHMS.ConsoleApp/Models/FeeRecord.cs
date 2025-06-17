321318e1c014cd212d15836388b159586ec633e3
pranave-ai <pranave.ai@gmail.com>
2025-06-17 15:49:55 +0530

    Updated Classes and Program.cs

--- SHMS.ConsoleApp/Models/FeeRecord.cs
+++ SHMS.ConsoleApp/Models/FeeRecord.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace SmartHostelManagementSystem.Models;
 
 public class FeeRecord
--- SHMS.ConsoleApp/Models/Student.cs
+++ SHMS.ConsoleApp/Models/Student.cs
@@ -1,15 +1,15 @@
-    namespace SmartHostelManagementSystem.Models;
+        namespace SmartHostelManagementSystem.Models;
 
-    public class Student : Person
-    {
-        public int RoomNumber { get; set; }
-        public bool FeePaid { get; set; }
-
-        public Student() { }
-
-        public Student(int id, string name, int roomNumber, bool feePaid) : base(id, name)
+        public class Student : Person
         {
-            RoomNumber = roomNumber;
-            FeePaid = feePaid;
+            public int RoomNumber { get; set; }
+            public bool FeePaid { get; set; }
+
+            public Student() { }
+
+            public Student(int id, string name, int roomNumber, bool feePaid) : base(id, name)
+            {
+                RoomNumber = roomNumber;
+                FeePaid = feePaid;
+            }
         }
-    }
